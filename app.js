d48940912c7d5c571ee935b4e4c43e4c0cbd9835
NgocVuu <ngocvu14.3.2001@gmail.com>
2023-06-27 20:00:37 +0700

    header, connect to mongodb

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ require ('dotenv').config();
 const express  = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db')
+
 const app  = express ();
 const PORT = 5000 || process.env.PORT;
+
+// connect to DB
+connectDB();
+
 // const res = require('express/lib/response');
 app.use(express.static('public'));
 
