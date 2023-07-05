8d5688afbed94cd5efd7d11de1d95a01e50f76f3
NgocVuu <ngocvu14.3.2001@gmail.com>
2023-07-05 09:48:45 +0700

    Search, Admin,

---
+++ server/routes/admin.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
+
+const adminLayout = '../views/layouts/admin';
+
+/*
+* GET /
+* Admin - Login Page
+*/
+router.get('/admin', async (req, res) => {
+    try {
+        const locals = {
+            title: "Admin",
+            description: "Simple Blog created with NodeJS, Express & MongoDB"
+        };
+
+        res.render('admin/index', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/*
+* POST /
+* Admin - Check Login
+*/
+router.post('/admin', async (req, res) => {
+    try {
+
+        res.render('admin/index', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
